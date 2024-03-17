922542e0dd7fdc36293b164ffcd1ae69aad308b9
Erik M. Buck <erik.buck@sbcglobal.net>
2024-03-17 19:28:22 -0400

    issue#1 (#58)
    Co-authored-by: erikbuck <erik.buck@wright.edu>

---
+++ Swiftipede/Swiftipede/Controller/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Swiftipede
+//
+//  Created by Erik M. Buck on 3/17/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+   var window: UIWindow?
+}
+
---
+++ Swiftipede/Swiftipede/Model/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Swiftipede
+//
+//  Created by Erik M. Buck on 3/17/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+        
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
