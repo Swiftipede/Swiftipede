e07f06d8289bcf81795092e622696707d9ba0741
Erik M. Buck <erik.buck@sbcglobal.net>
2024-03-18 12:49:05 -0400

    closes 5  implement calculations for grid of mushrooms andor other game items with appropriate dimensions for centipede like game (#61)
    * Implement issue#5 (provide coordinate conversions for game area grid mapped to scene frame
    
    * issue#5
    
    ---------
    
    Co-authored-by: erikbuck <erik.buck@wright.edu>

--- Swiftipede/Swiftipede/Model/GameScene.swift
+++ Swiftipede/Swiftipede/Model/GameScene.swift
@@ -9,10 +9,33 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    override func didMove(to view: SKView) {
-    }
-        
-    override func update(_ currentTime: TimeInterval) {
-    }
+   static let gameAreaWidth = Int32(25)
+   static let gameAreaHeight = Int32(40)
+   
+   /// \ref issue5 : Convert from game coordinates to scene coordinates
+   func convertGAXToSceneX(gaX : CGFloat) -> CGFloat {
+      return gaX * frame.width / CGFloat(GameScene.gameAreaWidth)
+      
+   }
+   
+   /// \ref issue5 : Convert from game coordinates to scene coordinates
+   func convertGAYToSceneY(gaY : CGFloat) -> CGFloat {
+      return gaY * frame.height / CGFloat(GameScene.gameAreaHeight)
+   }
+   
+   /// \ref issue5 : Convert from game coordinates to scene coordinates
+   func convertSceneXtoGAX(position: CGPoint) -> CGFloat {
+      return position.x * CGFloat(GameScene.gameAreaWidth) / frame.width
+   }
+   
+   /// \ref issue5 : Convert from game coordinates to scene coordinates
+   func convertSceneYtoGAY(position: CGPoint) -> CGFloat {
+      return position.y * CGFloat(GameScene.gameAreaHeight) / frame.height
+   }
+
+   override func didMove(to view: SKView) {
+   }
+   
+   override func update(_ currentTime: TimeInterval) {
+   }
 }
